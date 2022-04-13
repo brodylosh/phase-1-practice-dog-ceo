ad0c44d3f0082d68a38668939f7d29483cdae689
Brody Losh <brodylosh@gmail.com>
2022-04-13 10:01:27 -0400

    commit tree 0934288debcf1067325585810fda65f70140e0ec

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function () {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    const ul = document.getElementById('dog-breeds')
+    const li = document.createElement('li')
+    fetch(imgUrl)
+    .then(obj => obj.json())
+    .then(data => ul.appendChild(li.textContent(data)))
+    
+    
+    
+    
+    
+
+  });
